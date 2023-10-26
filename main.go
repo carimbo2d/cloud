d8e07639d4b30a25ea7b3b02ce4b4c9fd54d006b
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-10-26 09:12:54 -0300

    Work in progress

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ type SampleResponse struct {
 	Object ObjectSampleResponse `json:"object"`
 }
 
-func (s *HelperService) Sample() SampleResponse {
+func (s *HelperService) Sample(...any) SampleResponse {
 	return SampleResponse{
 		Integer: 1,
 		Boolean: true,
